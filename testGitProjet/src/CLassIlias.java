fbab54d7b78ae0d307af2cd9a1e355cf1d704e1a
Benncer Mohamed-ilias <benncer@a16p11.fil.univ-lille1.fr>
2015-11-20 09:10:34 +0100

    modif premiere partie

--- testGitProjet/src/CLassIlias.java
+++ testGitProjet/src/CLassIlias.java
@@ -1,5 +1,12 @@
 
 public class CLassIlias {
+/*Le Lorem Ipsum est simplement du faux
+ texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de te
+xte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté 
+la bureautique informatique, sans q
+ue son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des p
+assages du Lorem Ipsum, et, plus réc
+emment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMake*/
 	
 	public void affProduit(){
 		ClassIbrahim a = new ClassIbrahim();
